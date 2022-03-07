a06f648a5b913da9c3b9cf36df8b04d785fad9f8
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2022-03-08 00:44:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+START_MSG = os.environ.get("START_MESSAGE", "𝐻𝑒𝑙𝑙𝑜𝑜𝑜𝑜...😉{first}\n\n𝐼 𝑐𝑎𝑛 𝑠𝑡𝑜𝑟𝑒 𝑝𝑟𝑖𝑣𝑎𝑡𝑒 𝑓𝑖𝑙𝑒𝑠 𝑖𝑛 𝑆𝑝𝑒𝑐𝑖𝑓𝑖𝑒𝑑 𝐶ℎ𝑎𝑛𝑛𝑒𝑙 𝑎𝑛𝑑 𝑜𝑡ℎ𝑒𝑟 𝑢𝑠𝑒𝑟𝑠 𝑐𝑎𝑛 𝑎𝑐𝑐𝑒𝑠𝑠 𝑖𝑡 𝑓𝑟𝑜𝑚 𝑠𝑝𝑒𝑐𝑖𝑎𝑙 𝑙𝑖𝑛𝑘.")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -35,7 +35,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "𝐻𝑒𝑙𝑙𝑜𝑜𝑜𝑜...😉{first}\n\n<b>𝑌𝑜𝑢 𝑛𝑒𝑒𝑑 𝑡𝑜 𝑗𝑜𝑖𝑛 𝑚𝑦 𝑐ℎ𝑎𝑛𝑛𝑒𝑙 /𝑔𝑟𝑜𝑢𝑝 𝑎𝑛𝑑 𝑢𝑠𝑒 𝑚𝑒.. ☺️\n\n𝑃𝑙𝑒𝑎𝑠𝑒 𝑗𝑜𝑖𝑛 𝑚𝑒 𝑚𝑦 𝑐ℎ𝑎𝑛𝑛𝑒𝑙🤪</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
